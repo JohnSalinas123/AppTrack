a0828a50499ffd3c710c595063b13909b619c31c
JohnSalinas123 <salinasjohn257@gmail.com>
2024-05-22 18:19:58 -0700

    add login and getUser functions

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -3,6 +3,7 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import User, {IUser} from '../models/User';
 
+// register new user
 export const register = async (req: Request, res: Response): Promise<Response> => {
 
     const {email, password} = req.body;
@@ -24,4 +25,47 @@ export const register = async (req: Request, res: Response): Promise<Response> =
         return res.status(500).json({error: typedError.message})
     }
 
+}
+
+// login user
+export const login = async (req: Request, res: Response): Promise<Response> => {
+
+    const {email, password} = req.body;
+    try {
+        const user: IUser | null = await User.findOne({email})
+        if (!user) {
+            return res.status(400).json({message: "Incorrect email or password"})
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (!isMatch) {
+            return res.status(400).json({message: "Incorrect email or password"})
+        }
+
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET as string, {expiresIn: '1h'})
+        return res.json({token});
+    } catch (error: unknown) {
+        const typedError = error as Error;
+        return res.status(500).json({error: typedError.message})
+    }
+
+}
+
+// get user data
+export const getUser = async (req: Request, res: Response): Promise<Response> => {
+
+    const { id } = req.body
+
+    try {
+        const user: IUser | null = await User.findById(id).select('-password')
+        if (!user) {
+            return res.status(404).json({message: 'User not found'});
+        }
+
+        return res.json(user);
+
+    } catch(error) {
+        const typedEror = error as Error;
+        return res.status(500).json({error: typedEror.message})
+    }
 }
