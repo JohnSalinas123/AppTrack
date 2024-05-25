4861f21ba711d872b5134124b66ccc139d655d97
JohnSalinas123 <salinasjohn257@gmail.com>
2024-05-24 19:15:11 -0700

    fixed incompatible AuthenticatedRequest error, add getJobStatuses

--- backend/src/controllers/jobStatusController.ts
+++ backend/src/controllers/jobStatusController.ts
@@ -2,16 +2,19 @@ import { Request, Response } from "express";
 import { AuthenticatedRequest } from "../types/express";
 import JobStatus, { IJobStatus } from "../models/JobStatus";
 
+// add a single job status with a reference to current user
 export const addJobStatus = async (
-	req: AuthenticatedRequest,
+	req: Request,
 	res: Response
 ): Promise<Response> => {
 	const { name } = req.body;
 
 	try {
+		const user = (req as AuthenticatedRequest).user;
+
 		// check if status already exists for user
 		const existingStatus = await JobStatus.findOne({
-			user: req.user.id,
+			user: user._id,
 			name: name,
 		});
 		if (existingStatus) {
@@ -20,13 +23,39 @@ export const addJobStatus = async (
 
 		// create status object
 		const newJobStatus: IJobStatus = new JobStatus({
-			user: req.user._id,
+			user: user._id,
 			name,
 		});
 
 		// save new status of user to database
 		await newJobStatus.save();
 		return res.status(201).json(newJobStatus);
+	} catch (error: unknown) {
+		const typedError = error as Error;
+		return res.status(500).json({ error: typedError.message });
+	}
+};
+
+// get a user's job statuses
+export const getJobStatuses = async (
+	req: Request,
+	res: Response
+): Promise<Response> => {
+	try {
+		const userObj = (req as AuthenticatedRequest).user;
+
+		// get all jobstatus documents from jobstatus collection that ref current user
+		const jobStatuses = await JobStatus.find({ user: userObj._id });
+		if (!jobStatuses) {
+			return res.status(400).json({ message: "No statuses found" });
+		}
+
+		const jobStatusesWithoutUser = jobStatuses.map(({ _id, name }) => ({
+			_id,
+			name,
+		}));
+
+		return res.status(201).json(jobStatusesWithoutUser);
 	} catch (error: unknown) {
 		const typedError = error as Error;
 		return res.status(500).json({ error: typedError.message });
