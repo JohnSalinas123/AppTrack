243e768ab969301eeb4ff3d38faa594f912566e4
JohnSalinas123 <salinasjohn257@gmail.com>
2024-06-01 22:05:34 -0700

    add navigation for login page, auth checking wip

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,30 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { ApplicationPage } from "./pages/ApplicationPage";
 import { LayoutPage } from "./pages/LayoutPage";
+import { LoginPage } from "./pages/LoginPage";
+import { useState } from "react";
+import { NoPage } from "./pages/NoPage";
 
 function App() {
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+
 	return (
 		<>
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<LayoutPage />}>
-						<Route index element={<ApplicationPage />} />
-					</Route>
-				</Routes>
-			</BrowserRouter>
+			<Routes>
+				<Route path="/login" element={<LoginPage />} />
+				<Route path="/" element={<Navigate replace to="/applications" />} />
+				<Route
+					path="/applications"
+					element={
+						isLoggedIn ? <LayoutPage /> : <Navigate replace to="/login" />
+					}
+				>
+					<Route index element={<ApplicationPage />} />
+				</Route>
+				<Route path="*" element={<NoPage />} />
+			</Routes>
 		</>
 	);
 }
---
+++ frontend/src/pages/NoPage.tsx
@@ -0,0 +1,7 @@
+export const NoPage = () => {
+	return (
+		<>
+			<div>404 error</div>
+		</>
+	);
+};
