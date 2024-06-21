ab9ec23404aa062b04e17d531b3f4e1a008544ca
JohnSalinas123 <salinasjohn257@gmail.com>
2024-06-21 11:52:36 -0700

    add app notes button to show notes and allow editing, does not include modal yet

--- frontend/src/components/apptable/AppNotesButton.tsx
+++ frontend/src/components/apptable/AppNotesButton.tsx
@@ -1,9 +1,40 @@
-export const AppNotesButton = () => {
+import { FaRegNoteSticky } from "react-icons/fa6";
+import { MdNotes } from "react-icons/md";
+
+import "./AppNotesButton.css";
+import { useEffect, useState } from "react";
+
+interface AppNotesButtonProps {
+	notes: string;
+}
+
+export const AppNotesButton: React.FC<AppNotesButtonProps> = ({ notes }) => {
+	const [notesData, setNotesData] = useState<string>(notes);
+	const [showModal, setShowModal] = useState<boolean>(false);
+
+	useEffect(() => {
+		// set notes data state with notes prop
+		setNotesData(notes);
+	}, [notes]);
+
+	const handleButtonClick = () => {
+		// show notes modal form
+		setShowModal(true);
+		console.log(showModal);
+		console.log(notesData);
+	};
+
 	return (
 		<>
-			<div className="button-box">
-				<FaRegNoteSticky className="empty-notes-icon" />
-				<MdNotes className="notes-icon" />
+			<div className="button-container">
+				<button
+					type="button"
+					className={`button-box ${notesData.length == 0 ? "empty-notes" : ""}`}
+					onClick={handleButtonClick}
+				>
+					<FaRegNoteSticky className="empty-notes-icon" />
+					{notesData && <MdNotes className="notes-icon" />}
+				</button>
 			</div>
 		</>
 	);
